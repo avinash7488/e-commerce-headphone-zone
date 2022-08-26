bee59da5207c11abd920b17d349884527fa0986a
Mragendra Chaudhary <priyankchaudharyce@gmail.com>
2022-08-27 00:38:53 +0530

    all sorting anf filter & add to card enabled

--- scripts/product1.js
+++ scripts/product1.js
@@ -1,10 +1,5 @@
-import getData from "../component/search.js"
+import {getData,filter1,filter2,filter3,filter4,filter5,filter6,filter7,filter8,filter9,filter10,filter11,filter12,filter13,filter14,filter15,sort1,sort2,price1,price2,price3,price4,price5} from "../component/search.js"
 document.querySelector("body").onload=getData();
-
-// import filter1 from "../component/filter1.js"
-// {document.querySelector("#sony").addEventListener("click",()=>{
-//     filter1()
-// })
 
 let brand=document.createElement("div");
 document.querySelector("#brandbut2").addEventListener("click", function(){
@@ -14,10 +9,6 @@ let div=document.querySelector("#brand")
 brand.innerHTML=null;
 
 brand.innerHTML=`
-
-<form>
-<button id="sony">Sony</button>
-
 <input type="checkbox" id="brand1"  name="brand1">
 <label for="brand1">AKG (1)</label><br>
 <input type="checkbox" id="brand2" name="brand2">
@@ -42,12 +33,46 @@ brand.innerHTML=`
 <label for="brand11">Sony (2)</label><br>
 <input type="checkbox" id="brand12" name="brand12">
 <label for="brand12">V-MODA (6)</label><br>
-<input type="submit" id="submit" name="brand12">
-</form>
+
 `
 
 div.append(brand)
-
+document.getElementById('brand1').addEventListener("click", function() {
+    filter1()
+})
+document.getElementById('brand2').addEventListener("click", function() {
+    filter2()
+})
+document.getElementById('brand3').addEventListener("click", function() {
+    filter3()
+})
+document.getElementById('brand4').addEventListener("click", function() {
+    filter4()
+})
+document.getElementById('brand5').addEventListener("click", function() {
+    filter5()
+})
+document.getElementById('brand6').addEventListener("click", function() {
+     filter6()
+})
+document.getElementById('brand7').addEventListener("click", function() {
+    filter7()
+})
+document.getElementById('brand8').addEventListener("click", function() {
+    filter8()
+})
+document.getElementById('brand9').addEventListener("click", function() {
+    filter9()
+})
+document.getElementById('brand10').addEventListener("click", function() {
+    filter10()
+})
+document.getElementById('brand11').addEventListener("click", function() {
+    filter11()
+})
+document.getElementById('brand12').addEventListener("click", function() {
+    filter12()
+})
 document.querySelector("#brandbut1").addEventListener("click",()=>{
     brand.innerHTML=null;
     window.location.reload();
@@ -64,10 +89,23 @@ document.querySelector("#productbut2").addEventListener("click", function(){
     let div=document.querySelector("#product")
     cat.innerHTML=null;
     cat.innerHTML=`
-    <input type="checkbox" id="wired" name="wired">Wired Headphone
+    <input type="checkbox" id="head" name="wired">Headphone
+  <br>
+  <input type="checkbox" id="ear" name="wired">Wireless Earbuds
+  <br>
+  <input type="checkbox" id="earphones" name="wired">Earphones
   <br>
     `
     div.append(cat)
+    document.getElementById('head').addEventListener("click", function() {
+        filter13()
+    })
+    document.getElementById('ear').addEventListener("click", function() {
+        filter15()
+    })
+    document.getElementById('earphones').addEventListener("click", function() {
+        filter14()
+    })
     document.querySelector("#productbut1").addEventListener("click",()=>{
         cat.innerHTML=null;
         window.location.reload();
@@ -82,6 +120,12 @@ document.querySelector("#pricebut2").addEventListener("click", () => {
     div1.innerHTML=`<button id="htl">High to low</button>
     <button id="lth">Low to high</button>`
     price.append(div1)
+    document.getElementById('lth').addEventListener("click", function() {
+        sort1()
+    })
+    document.getElementById('htl').addEventListener("click", function() {
+        sort2()
+    })
     document.querySelector("#pricebut1").addEventListener("click",()=>{
         price.innerHTML=null;
         window.location.reload();
@@ -104,4 +148,19 @@ document.querySelector("#availbut1").addEventListener("click",()=>{
     avail.innerHTML=null;
     window.location.reload();
 })
+})
+document.querySelector("#price1").addEventListener("click",()=>{
+    price1()
+})
+document.querySelector("#price2").addEventListener("click",()=>{
+    price2()
+})
+document.querySelector("#price3").addEventListener("click",()=>{
+    price3()
+})
+document.querySelector("#price4").addEventListener("click",()=>{
+    price4()
+})
+document.querySelector("#price5").addEventListener("click",()=>{
+    price5()
 })
