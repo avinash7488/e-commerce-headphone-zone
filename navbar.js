f9efd27a619e8cbd072cd4d0fe383b90fdeed7e7
avinash kumar <avi064448@gmail.com>
2022-08-27 20:55:33 +0530

    navbar and footer file

--- navbar.js
+++ navbar.js
@@ -21,74 +21,75 @@ export function navbar(){
         <li>
             <p>Category</p>
             <div class="nav_sub_menu">
-                <table >
-                    <tr>
-                        <td>
-                            <img src="https://cdn-icons-png.flaticon.com/512/3566/3566959.png" alt="">
-                            <p>IN-EARS FOR BEGINNERS</p>
-                        </td>
-                        <td>
-                            <img src="https://cdn-icons-png.flaticon.com/512/8019/8019048.png" alt="">
-                            <p>HEADPHONES FOR BEGINNERS</p>
-                        </td>
-                        <td> <img src="https://cdn-icons-png.flaticon.com/512/5681/5681325.png" alt="">
-                            <p>TRUE WIRELESS EARBUDS</p></td>
-                        <td>      
-                            <img src="https://cdn-icons-png.flaticon.com/512/3206/3206786.png" alt="">
-                            <p>WIRELESS HEADPHONES</p></td>
-                        <td>
-                            <img src="https://cdn-icons-png.flaticon.com/512/8284/8284984.png" alt="">
-                            <p>FLAGSHIP IEMS</p>
-                        </td>
-                        <td>
-                            <img src="https://cdn-icons-png.flaticon.com/512/8305/8305607.png" alt="">
-                            <p>FLAGSHIP HEADPHONES</p>
-                        </td>
-                        <td>
-                            <img src="https://cdn-icons-png.flaticon.com/512/1544/1544805.png" alt="">
-                            <p>HI-RES AUDIO PLAYERS</p>
-                        </td>
-                        <td>
-                            <img src="https://cdn-icons-png.flaticon.com/512/7386/7386518.png" alt="">
-                            <p>PORTABLE AMPS & DACS</p>
-                        </td>
-                    </tr>
-                    
-                    <tr>
-                        <td>
-                            <img src="https://cdn-icons-png.flaticon.com/512/2095/2095240.png" alt="">
-                            <p>DESKTOP AMPS & DACS</p>
-                        </td>
-                        <td>
-                            <img src="https://cdn-icons-png.flaticon.com/512/2618/2618974.png" alt="">
-                            <p>WORK FROM HOME</p>
-                        </td>
-                        <td>
-                            <img src="https://cdn-icons-png.flaticon.com/512/3142/3142080.png" alt="">
-                            <p>GAMING</p>
-                        </td>
-                        <td>
-                            <img src="https://cdn-icons-png.flaticon.com/512/3613/3613243.png" alt="">
-                            <p>STUDIO & PROFESSIONAL</p>
-                        </td>
-                        <td>
-                            <img src="https://cdn-icons-png.flaticon.com/512/7919/7919605.png" alt="">
-                            <p>SPEAKERS</p>
-                        </td>
-                        <td>
-                            <img src="https://cdn-icons-png.flaticon.com/512/6323/6323608.png" alt="">
-                            <p>CABLES</p>
-                        </td>
-                        <td>
-                            <img src="https://cdn-icons-png.flaticon.com/512/6907/6907871.png" alt="">
-                            <p>EARTIPS & EARPADS</p>
-                        </td>
-                        <td>
-                            <img src="https://cdn-icons-png.flaticon.com/512/8051/8051228.png" alt="">
-                            <p>CASES</p>
-                        </td>
-                    </tr>
-                </table>
+            <table id="category_sec">
+            <tr>
+                <td onclick="getthis_data1()">
+                    <img src="https://cdn-icons-png.flaticon.com/512/3566/3566959.png" alt="">
+                    <p>IN-EARS FOR BEGINNERS</p>
+                </td>
+                <td onclick="getthis_data2()">
+                    <img src="https://cdn-icons-png.flaticon.com/512/8019/8019048.png" alt="">
+                    <p>HEADPHONES FOR BEGINNERS</p>
+                </td>
+                <td onclick="getthis_data3()"> 
+                    <img src="https://cdn-icons-png.flaticon.com/512/5681/5681325.png" alt="">
+                    <p>TRUE WIRELESS EARBUDS</p></td>
+                <td onclick="getthis_data4()">      
+                    <img src="https://cdn-icons-png.flaticon.com/512/3206/3206786.png" alt="">
+                    <p>WIRELESS HEADPHONES</p></td>
+                <td onclick="getthis_data5()">
+                    <img src="https://cdn-icons-png.flaticon.com/512/8284/8284984.png" alt="">
+                    <p>FLAGSHIP IEMS</p>
+                </td>
+                <td onclick="getthis_data1()">
+                    <img src="https://cdn-icons-png.flaticon.com/512/8305/8305607.png" alt="">
+                    <p>FLAGSHIP HEADPHONES</p>
+                </td>
+                <td onclick="getthis_data1()">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1544/1544805.png" alt="">
+                    <p>HI-RES AUDIO PLAYERS</p>
+                </td>
+                <td onclick="getthis_data1()">
+                    <img src="https://cdn-icons-png.flaticon.com/512/7386/7386518.png" alt="">
+                    <p>PORTABLE AMPS & DACS</p>
+                </td>
+            </tr>
+            
+            <tr>
+                <td onclick="getthis_data1()">
+                    <img src="https://cdn-icons-png.flaticon.com/512/2095/2095240.png" alt="">
+                    <p>DESKTOP AMPS & DACS</p>
+                </td>
+                <td onclick="getthis_data6()">
+                    <img src="https://cdn-icons-png.flaticon.com/512/2618/2618974.png" alt="">
+                    <p>WORK FROM HOME</p>
+                </td>
+                <td onclick="getthis_data1()">
+                    <img src="https://cdn-icons-png.flaticon.com/512/3142/3142080.png" alt="">
+                    <p>GAMING</p>
+                </td>
+                <td onclick="getthis_data1()">
+                    <img src="https://cdn-icons-png.flaticon.com/512/3613/3613243.png" alt="">
+                    <p>STUDIO & PROFESSIONAL</p>
+                </td>
+                <td onclick="getthis_data1()">
+                    <img src="https://cdn-icons-png.flaticon.com/512/7919/7919605.png" alt="">
+                    <p>SPEAKERS</p>
+                </td>
+                <td onclick="getthis_data1()">
+                    <img src="https://cdn-icons-png.flaticon.com/512/6323/6323608.png" alt="">
+                    <p>CABLES</p>
+                </td>
+                <td onclick="getthis_data1()">
+                    <img src="https://cdn-icons-png.flaticon.com/512/6907/6907871.png" alt="">
+                    <p>EARTIPS & EARPADS</p>
+                </td>
+                <td onclick="getthis_data1()">
+                    <img src="https://cdn-icons-png.flaticon.com/512/8051/8051228.png" alt="">
+                    <p>CASES</p>
+                </td>
+            </tr>
+        </table>
             </div>
       </li>
 
@@ -97,11 +98,11 @@ export function navbar(){
             <div class="nav_sub_menu" id="sub_menu_brands">
                 <table>
                     <tr>
-                        <td><p>AUDIO-TECHNICA</p></td>
-                        <td><p>SONY</p></td>
-                        <td><p>CCA</p></td>
-                        <td><p>FINAL</p></td>
-                        <td><p>7HZ</p></td>
+                        <td onclick="getBrand1()"><p>AUDIO-TECHNICA</p></td>
+                        <td onclick="getBrand2()"><p>SONY</p></td>
+                        <td onclick="getBrand3()"><p>BEYERDYNAMIC</p></td>
+                        <td onclick="getBrand4()"><p>HIFIMAN</p></td>
+                        <td onclick="getBrand5()"><p>SENNHEISER</p></td>
                     </tr>
 
                     <tr>
@@ -109,13 +110,13 @@ export function navbar(){
                         <td><p>CAMPFIRE AUDIO</p></td>
                         <td><p>TIMELESS</p></td>
                         <td><p>MEZE</p></td>
-                        <td><p>HIFIMAN</p></td>
+                        <td><p>FINAL</p></td>
                     </tr>
 
                     <tr>
-                        <td><p>BEYERDYNAMIC</p></td>
+                        <td><p>CCA</p></td>
                         <td><p>LYPERTEK</p></td>
-                        <td><p>SENNHEISER</p></td>
+                        <td><p>7HZ</p></td>
                         <td><p>SHURE</p></td>
                         <td><p>V-MODA</p></td>
                     </tr>
@@ -175,23 +176,24 @@ export function navbar(){
                 </div>
             </div>
         </li>
-        <li id="helpcenter"><a href="https://help.headphonezone.in/support/home?_gl=1*c1jlxr*_ga*MTQ1NzE1NzA0LjE2NjEyMzMyNTk.*_ga_G4MJRJ9C7G*MTY2MTYwNDMyMi4yMi4xLjE2NjE2MDQzMjkuNTMuMC4w"> Help Center</a></li>
+        <li id="helpcenter"><a href="https://help.headphonezone.in/support/home?_gl=1*c1jlxr*_ga*MTQ1NzE1NzA0LjE2NjEyMzMyNTk.*_ga_G4MJRJ9C7G*MTY2MTYwNDMyMi4yMi4xLjE2NjE2MDQzMjkuNTMuMC4w"><p> Help Center</p></a></li>
         
     </ul>
 </div>
 
 <div id="right_menu">
     
-        <p id="nav_search">Search</p>
-       
-    
-    
+<input type="text" id="nav_search" placeholder="Search">
         <p>Login</p>
-    
-    <p>cart</p>
+        <p>cart</p>
       
       
       
       <p id="nav_quant_cart">0</p>
 </div>`
 }
+
+
+
+
+
---
+++ navfun.js
@@ -0,0 +1,78 @@
+let getthis_data1 = () =>{
+
+    localStorage.setItem("pro_page_key",("earpthone"));
+    //window.location.href="";
+
+};
+
+let getthis_data2 = () => {
+
+    localStorage.setItem("pro_page_key",("headphone"));
+//window.location.href="";
+
+};
+
+let getthis_data3 = () => {
+
+    localStorage.setItem("pro_page_key",("wireless earbuds"));
+    //window.location.href="";
+
+};
+
+let getthis_data4 = () => {
+
+    localStorage.setItem("pro_page_key",("wireless headphones"));
+//window.location.href="";
+
+};
+
+let getthis_data5 = () => {
+
+    localStorage.setItem("pro_page_key",("earphone"));
+//window.location.href="";
+
+}
+
+let getthis_data6 = () => {
+
+    localStorage.setItem("pro_page_key",("work from home"));
+//window.location.href="";
+
+}
+
+
+
+
+//brand
+
+let getBrand1 = () => {
+
+    localStorage.setItem("pro_page_key",("AUDIO-TECHNICA"));
+//window.location.href="";
+
+}
+
+let getBrand2 = () => {
+
+    localStorage.setItem("pro_page_key",("SONY"));
+//window.location.href="";
+
+}
+let getBrand3 = () => {
+
+    localStorage.setItem("pro_page_key",("BEYERDYNAMIC"));
+//window.location.href="";
+
+}
+let getBrand4 = () => {
+
+    localStorage.setItem("pro_page_key",("HIFIMAN"));
+//window.location.href="";
+
+}
+let getBrand5 = () => {
+
+    localStorage.setItem("pro_page_key",("SENNHEISER"));
+//window.location.href="";
+
+}
