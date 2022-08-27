bb66c8fbee15ffb33930c94c032f68cf66f5fd2c
avinash kumar <avi064448@gmail.com>
2022-08-27 19:22:37 +0530

    navbar file done

---
+++ navbar.js
@@ -0,0 +1,197 @@
+// document.getElementById('logo').addEventListener(click,function(){
+//     window.location.herf='./#'
+// })
+
+// document.getElementById('nav_search').addEventListener(click,function(){
+//     document.getElementById('search_div').style.visibility="visible";
+//     document.getElementById('search_div').style.transition="all 0.5s ease"
+// })
+
+
+export function navbar(){
+    return`<div id="logo">
+       
+
+    <img src="https://assets.rha-audio.com/nzssx/c/Headphone_zone_logo_3-small.png" alt="">
+  </div>
+
+<div class="nav_mega_menu">
+
+    <ul>
+        <li>
+            <p>Category</p>
+            <div class="nav_sub_menu">
+                <table >
+                    <tr>
+                        <td>
+                            <img src="https://cdn-icons-png.flaticon.com/512/3566/3566959.png" alt="">
+                            <p>IN-EARS FOR BEGINNERS</p>
+                        </td>
+                        <td>
+                            <img src="https://cdn-icons-png.flaticon.com/512/8019/8019048.png" alt="">
+                            <p>HEADPHONES FOR BEGINNERS</p>
+                        </td>
+                        <td> <img src="https://cdn-icons-png.flaticon.com/512/5681/5681325.png" alt="">
+                            <p>TRUE WIRELESS EARBUDS</p></td>
+                        <td>      
+                            <img src="https://cdn-icons-png.flaticon.com/512/3206/3206786.png" alt="">
+                            <p>WIRELESS HEADPHONES</p></td>
+                        <td>
+                            <img src="https://cdn-icons-png.flaticon.com/512/8284/8284984.png" alt="">
+                            <p>FLAGSHIP IEMS</p>
+                        </td>
+                        <td>
+                            <img src="https://cdn-icons-png.flaticon.com/512/8305/8305607.png" alt="">
+                            <p>FLAGSHIP HEADPHONES</p>
+                        </td>
+                        <td>
+                            <img src="https://cdn-icons-png.flaticon.com/512/1544/1544805.png" alt="">
+                            <p>HI-RES AUDIO PLAYERS</p>
+                        </td>
+                        <td>
+                            <img src="https://cdn-icons-png.flaticon.com/512/7386/7386518.png" alt="">
+                            <p>PORTABLE AMPS & DACS</p>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            <img src="https://cdn-icons-png.flaticon.com/512/2095/2095240.png" alt="">
+                            <p>DESKTOP AMPS & DACS</p>
+                        </td>
+                        <td>
+                            <img src="https://cdn-icons-png.flaticon.com/512/2618/2618974.png" alt="">
+                            <p>WORK FROM HOME</p>
+                        </td>
+                        <td>
+                            <img src="https://cdn-icons-png.flaticon.com/512/3142/3142080.png" alt="">
+                            <p>GAMING</p>
+                        </td>
+                        <td>
+                            <img src="https://cdn-icons-png.flaticon.com/512/3613/3613243.png" alt="">
+                            <p>STUDIO & PROFESSIONAL</p>
+                        </td>
+                        <td>
+                            <img src="https://cdn-icons-png.flaticon.com/512/7919/7919605.png" alt="">
+                            <p>SPEAKERS</p>
+                        </td>
+                        <td>
+                            <img src="https://cdn-icons-png.flaticon.com/512/6323/6323608.png" alt="">
+                            <p>CABLES</p>
+                        </td>
+                        <td>
+                            <img src="https://cdn-icons-png.flaticon.com/512/6907/6907871.png" alt="">
+                            <p>EARTIPS & EARPADS</p>
+                        </td>
+                        <td>
+                            <img src="https://cdn-icons-png.flaticon.com/512/8051/8051228.png" alt="">
+                            <p>CASES</p>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+      </li>
+
+        <li>
+            <p>Brands</p>
+            <div class="nav_sub_menu" id="sub_menu_brands">
+                <table>
+                    <tr>
+                        <td><p>AUDIO-TECHNICA</p></td>
+                        <td><p>SONY</p></td>
+                        <td><p>CCA</p></td>
+                        <td><p>FINAL</p></td>
+                        <td><p>7HZ</p></td>
+                    </tr>
+
+                    <tr>
+                        <td><p>BLON</p></td>
+                        <td><p>CAMPFIRE AUDIO</p></td>
+                        <td><p>TIMELESS</p></td>
+                        <td><p>MEZE</p></td>
+                        <td><p>HIFIMAN</p></td>
+                    </tr>
+
+                    <tr>
+                        <td><p>BEYERDYNAMIC</p></td>
+                        <td><p>LYPERTEK</p></td>
+                        <td><p>SENNHEISER</p></td>
+                        <td><p>SHURE</p></td>
+                        <td><p>V-MODA</p></td>
+                    </tr>
+                </table>
+            </div>
+        </li>
+        <li>
+            <p>Price</p>
+          <div class="nav_sub_menu" id="sub_menu_price">
+            <tt>	
+            <p>Best Headphones Under Rs 1,000</p>
+            <p>Best Headphones Under Rs 2,000</p>
+            <p>Best Headphones Under Rs 3,000</p>
+            <p>Best Headphones Under Rs 5,000</p>
+            <p>Best Headphones Under Rs 10,000</p>
+            <p>Best Headphones Under Rs 20,000</p>
+            <p>Best Headphones Under Rs 30,000</p>
+            
+           </tt>       
+        </li>
+        <li><p>Deals</p></li>
+        <li><p>Headphone Finder</p></li>
+        <li><p>Events</p></li>
+        <li><p>Forum</p></li>
+        <li><p>Info</p>
+            <div class="nav_sub_menu" id="sub_menu_info">
+                <div class="col">
+                    <img src="https://cdn.shopify.com/s/files/1/0153/8863/files/Headphone-Zone-10-Years_500x-1_295x.jpg?v=1643031208" alt="">
+                    <h4>HISTORY OF HEADPHONE ZONE</h4>
+                    <p>Read the Headphone Zone journey since 2011 as told by Raghav.</p>
+                </div>
+                <div class="col">
+                    <img src="https://cdn.shopify.com/s/files/1/0153/8863/files/Headphone-Zone-Club-Audiophile-Menu_500x-focal_256x256_c41abc2b-75a9-4aeb-b2cf-4f01ae7c1992_256x.webp?v=1650019007" alt="">
+                    <h4>IT PLAYS TO BE AN AUDIOPHILE</h4>
+                    <p>Enjoy Members-only rewards with our loyalty program</p>
+                </div>
+                <div class="col">
+                    <h4>ABOUT US</h4>
+                    <p>Our Story</p>
+                    <P>Our Values</P>
+                    <P>Meet the Team</P>
+                    <P>Customer Testimonials</P>
+                    <P>Careers-We're Hiring!</P>
+                    <P>Brands That Inspire Us</P>
+                </div>
+                <div class="col">
+                    <h4>GET IN TOUCH</h4>
+                    <p>Contact the Team</p>
+                    <p>Corporate Gifting</p>
+                    <P>Partner With Us</P>
+                </div>
+                <div class="col">
+                    <h4>COMMUNITY</h4>
+                    <p>Headphone Zone Merchandise</p>
+                    <p>Tread-Up Program</p>
+                    <P>Affiliate Program</P>
+                </div>
+            </div>
+        </li>
+        <li id="helpcenter"><a href="https://help.headphonezone.in/support/home?_gl=1*c1jlxr*_ga*MTQ1NzE1NzA0LjE2NjEyMzMyNTk.*_ga_G4MJRJ9C7G*MTY2MTYwNDMyMi4yMi4xLjE2NjE2MDQzMjkuNTMuMC4w"> Help Center</a></li>
+        
+    </ul>
+</div>
+
+<div id="right_menu">
+    
+        <p id="nav_search">Search</p>
+       
+    
+    
+        <p>Login</p>
+    
+    <p>cart</p>
+      
+      
+      
+      <p id="nav_quant_cart">0</p>
+</div>`
+}
